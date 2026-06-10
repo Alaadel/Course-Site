3123024486c994f5abf388b61e3a40883d6d19cd
ALX <alaadel@outlook.com>
2026-06-10 12:10:40 +0300

    simple route flow test

---
+++ courses-app/app/authentication/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Authentication() {
+    return (
+        <>
+            <h1>Authentication Page</h1>
+            <Link href="/">Go back to Home</Link>
+        </>
+    );
+}
